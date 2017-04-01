4331d0aa51ead885a58a890b1c3e11e67c49e603
Connor Borden <connorbo97@gmail.com>
2017-04-01 10:54:24 -0700

    Checkbox Functionality

--- la_hacks/content.js
+++ la_hacks/content.js
@@ -1,2 +1,51 @@
-//content.js
-//alert("Hello from your extension's javascript")
+
+window.onload = onPageLoad();
+
+function onPageLoad() {
+	if (typeof(Storage) !== "undefined") {
+	if(localStorage.using == undefined)
+	{
+		localStorage.setItem("using", 0);
+	}else{
+		
+	}
+} else {
+    // Sorry! No Web Storage support..
+}
+  if(localStorage.getItem("using") == 0)
+  {
+		document.getElementById("power").checked = 0;
+  }
+  else
+  {
+		document.getElementById("power").checked = 1;
+  }
+}
+
+
+	var messWithPeeps = document.getElementById("power").checked;
+if(messWithPeeps)
+{
+	alert("mess with peeps");
+}
+function messWithCongress()
+{
+	
+	var check = document.getElementById("power").checked;
+	if(check == false)
+  {
+	localStorage.setItem("using", 0);
+  }
+  else
+  {
+	localStorage.setItem("using", 1);
+  }
+	  
+	if(check == true)
+	{
+		
+		alert("Button has been pressed");	
+		//this is where we can put shit to run if the button is checked
+		
+	}
+}
