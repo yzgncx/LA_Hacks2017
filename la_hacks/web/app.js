c224ffe28996ea7e25dd35afb300a6d37e6e93e3
yzgncx <tyler.w.carson@gmail.com>
2017-04-02 07:54:46 -0700

    attempted merge.

--- la_hacks/web/app.js
+++ la_hacks/web/app.js
@@ -1,71 +1,81 @@
 var Twitter = require("node-twitter-api");
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var _oauthVerifier;
+var _oauthToken;
+var _requestToken;
+var _requestSecret;
+var _accessToken;
+var _accessSecret;
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
- var twitter = new Twitter({
-        consumerKey: "SUBxCtGE3XL2k1TGF6ERNIh0S",
-    	consumerSecret: "adLtZk2nvylcUcA7B3DBJewyZqNYC55fmZ9hYVp3r6nJW8dQQI",
-    	callback: "http://localhost:3000/twitter-done"
-    });
-
-	app.get("/twitter-done", function(req,res) {
-		var token = req.query.oauth_token;
-		console.log(token);
-		res.send("okay")
-	})
-    var _requestSecret;
-
-    app.get("/request-token", function(req, res) {
-        twitter.getRequestToken(function(err, requestToken, requestSecret) {
-            if (err)
-                res.status(500).send(err);
-            else {
-                _requestSecret = requestSecret;
-                res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
-            }
-        });
-    });
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+var twitter = new Twitter({
+    consumerKey: "Dyc0IRbGT7xfdAqJF4ilauUfp",
+   	consumerSecret: "uivRi9QL2nqP3gnu5SbofCYP6NZ9L8Oe5aLQvJEzcnzT4j46lw",
+   	callback: "http://localhost:3000/twitter-done"
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+twitter.getRequestToken(function(err, requestToken, requestSecret) {
+	if (err) {
+	//	res.status(500).send(err);
+    }
+	else {
+		_requestToken = requestToken;
+        _requestSecret = requestSecret;
+		console.log("          retrieved request token and secret           ");
+		console.log(requestToken);
+		console.log(requestSecret);
+        //res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
+		//console.log("after res.redirect");
+	    }
+	});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+app.get("/twitter-done", function(req,res) {
+	console.log("PART 1:        in apt.get /twitter-done")
+	_oauthVerifier = req.query.oauth_verifier;
+	_requestToken = req.query.oauth_token;
+	
+	
+	console.log("     oauth_verifier");
+	console.log(_oauthVerifier);
+	twitter.getAccessToken(_requestToken, _requestSecret, _oauthVerifier, function(error, _accessToken, _accessSecret, results) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log("      access token and secret")
+			console.log(_accessToken);
+			console.log(_accessSecret);
+			
+			//res.redirect("http://localhost:3000/login")
+			res.redirect("http://localhost:3000/public/signin.html");
+			tweet();
+			//Step 4: Verify Credentials belongs here 
+		}
+	});
 });
+
+app.get("/login", function(req, res){
+	res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + _requestToken);
+});
+
+function tweet() {	
+	console.log("      in tweet function");
+	twitter.statuses("update", {
+			status: "Hello world!"
+		},
+		_accessToken,
+		_accessSecret,
+		function(error, data, response) {
+			if (error) {
+				console.log("      error");
+				console.log(error);
+				// something went wrong 
+			} else {
+				// data contains the data sent by twitter 
+			}
+		});
+}
 
 module.exports = app;
