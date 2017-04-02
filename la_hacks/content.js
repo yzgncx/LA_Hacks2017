9ac28b907983aef424c2f934d0075133a7f2dab7
yzgncx <tyler.w.carson@gmail.com>
2017-04-01 17:38:59 -0700

    added content.js file -- for some reason missed in previous commit

--- la_hacks/content.js
+++ la_hacks/content.js
@@ -1,2 +1,32 @@
-//content.js
-//alert("Hello from your extension's javascript")
+var https = require('https');
+
+    
+	function getToken(token,callback){
+        console.log("get token");
+	  var options = {
+	    host: 'api.twitter.com',
+	    path: '/oauth2/token',
+	    method: 'POST',
+	    headers: {'Authorization': 'Basic ' + token,
+	   'Content-Type': 'application/x-www-form-urlencoded'}
+	  };
+
+	  var req = https.request(options, function(response) {
+	    var str = ''
+	    response.on('data', function (chunk) {
+	      str += chunk;
+	    });
+
+	    response.on('end', function () {
+	      callback(JSON.parse(str));
+	    });
+	  });
+
+	  req.write("grant_type=client_credentials");
+	  req.end();
+	}
+
+var credential = new Buffer("SUBxCtGE3XL2k1TGF6ERNIh0S" + ":" + "adLtZk2nvylcUcA7B3DBJewyZqNYC55fmZ9hYVp3r6nJW8dQQI").toString('base64');
+getToken(credential, function(response) {
+	console.log(response);
+});
